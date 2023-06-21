13d1f712649ce28f168750c39d170a5e2893db3d
Keyyard <keyyard8888@gmail.com>
2023-06-21 18:29:44 +0700

    v3

--- PPT DAY CUNG/day cung v2.py
+++ PPT DAY CUNG/day cung v2.py
@@ -1,78 +1,78 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import math
-import PySimpleGUI as sg
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import math
+    import PySimpleGUI as sg
 
-def f(x):
-    return x**3 + x - 5
-layout = [
-    [sg.Text("Nhap a: ")],
-    [sg.Input(key="a",default_text="1")],
-    [sg.Text("Nhap b:")],
-    [sg.Input(key="b",default_text="2")],
-    #choose between Epsi and Steps
-    #[sg.Text("Nhap So Buoc:"),sg.Input(key="steps",default_text="10",size=(30,10),expand_x=True)]
-    #[sg.Text("Nhap Epsi:"),sg.Input(key="epsi",default_text="0.00001",size=(30,10),expand_x=True)],
-    [sg.Combo(["Epsi","Steps"],key="choose",default_value="Epsi",size=(10,10),enable_events=True)],
-    [sg.Text("Nhap Epsi:",visible=True,key="epsi_text"),sg.Input(key="epsi",default_text="0.00001",size=(30,10),expand_x=True,visible=True)],
-    [sg.Text("Nhap So Buoc:",visible=False,key="steps_text"),sg.Input(key="steps",default_text="10",size=(30,10),expand_x=True,visible=False)],
-    [sg.Button("RUN",key="run")],
-    [sg.Text(key="result")],
-    [sg.Text(text="by Trinh Minh Hieu - 11222359",justification="right",size=(50,0),font=("Arial",8))]
-]
-window = sg.Window("PPT: Day Cung",layout)
-while True:
-    event,values = window.read()
-    if event == sg.WIN_CLOSED:
-        break
-    a = round(float(values["a"]),2)
-    b = round(float(values["b"]),2)
-    if f(a) * f(b) > 0:
-        result = "Khong co nghiem trong khoang nay"
-        window["result"].update(result)
-        continue
-    if values["choose"] == "Steps":
-        window["steps_text"].update(visible=True)
-        window["epsi_text"].update(visible=False)
-        window["steps"].update(visible=True)
-        window["epsi"].update(visible=False)
-        steps = int(values["steps"])
-        print (steps)
-        if event == "run":
-            x = a - (b - a) * f(a) / (f(b) - f(a))
-            if f(x) * f(a) < 0:
-                for i in range(steps):
-                    b = x
-                    x = a - (b - a) * f(a) / (f(b) - f(a))
-                    #print(a, b, x, f(x))
-            else:
-                for i in range(steps):
-                    a = x
-                    x = a - (b - a) * f(a) / (f(b) - f(a))
-                    #print(a, b, x, f(x))
-                print (x)
-                result = "x = " + str(x)
-                window["result"].update(result)
-    if values["choose"] == "Epsi":
-        window["steps_text"].update(visible=False)
-        window["epsi_text"].update(visible=True)
-        window["steps"].update(visible=False)
-        window["epsi"].update(visible=True)
-        epsi = float(values["epsi"])
-        print (epsi)
-        if event == "run":
-            x = a - (b - a) * f(a) / (f(b) - f(a))
-            if f(x) * f(a) < 0:
-                while abs(x - b) > epsi:
-                    b = x
-                    x = a - (b - a) * f(a) / (f(b) - f(a))
-                    #print(a, b, x, f(x))
-            else:
-                while abs(x - a) > epsi:
-                    a = x
-                    x = a - (b - a) * f(a) / (f(b) - f(a))
-                    #print(a, b, x, f(x))
-                print (x)
-                result = "x = " + str(x)
-                window["result"].update(result)
+    def f(x):
+        return x**3 + x - 5
+    layout = [
+        [sg.Text("Nhap a: ")],
+        [sg.Input(key="a",default_text="1")],
+        [sg.Text("Nhap b:")],
+        [sg.Input(key="b",default_text="2")],
+        #choose between Epsi and Steps
+        #[sg.Text("Nhap So Buoc:"),sg.Input(key="steps",default_text="10",size=(30,10),expand_x=True)]
+        #[sg.Text("Nhap Epsi:"),sg.Input(key="epsi",default_text="0.00001",size=(30,10),expand_x=True)],
+        [sg.Combo(["Epsi","Steps"],key="choose",default_value="Epsi",size=(10,10),enable_events=True)],
+        [sg.Text("Nhap Epsi:",visible=True,key="epsi_text"),sg.Input(key="epsi",default_text="0.00001",size=(30,10),expand_x=True,visible=True)],
+        [sg.Text("Nhap So Buoc:",visible=False,key="steps_text"),sg.Input(key="steps",default_text="10",size=(30,10),expand_x=True,visible=False)],
+        [sg.Button("RUN",key="run")],
+        [sg.Text(key="result")],
+        [sg.Text(text="by Trinh Minh Hieu - 11222359",justification="right",size=(50,0),font=("Arial",8))]
+    ]
+    window = sg.Window("PPT: Day Cung",layout)
+    while True:
+        event,values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        a = round(float(values["a"]),2)
+        b = round(float(values["b"]),2)
+        if f(a) * f(b) > 0:
+            result = "Khong co nghiem trong khoang nay"
+            window["result"].update(result)
+            continue
+        if values["choose"] == "Steps":
+            window["steps_text"].update(visible=True)
+            window["epsi_text"].update(visible=False)
+            window["steps"].update(visible=True)
+            window["epsi"].update(visible=False)
+            steps = int(values["steps"])
+            print (steps)
+            if event == "run":
+                x = a - (b - a) * f(a) / (f(b) - f(a))
+                if f(x) * f(a) < 0:
+                    for i in range(steps):
+                        b = x
+                        x = a - (b - a) * f(a) / (f(b) - f(a))
+                        #print(a, b, x, f(x))
+                else:
+                    for i in range(steps):
+                        a = x
+                        x = a - (b - a) * f(a) / (f(b) - f(a))
+                        #print(a, b, x, f(x))
+                    print (x)
+                    result = "x = " + str(x)
+                    window["result"].update(result)
+        if values["choose"] == "Epsi":
+            window["steps_text"].update(visible=False)
+            window["epsi_text"].update(visible=True)
+            window["steps"].update(visible=False)
+            window["epsi"].update(visible=True)
+            epsi = float(values["epsi"])
+            print (epsi)
+            if event == "run":
+                x = a - (b - a) * f(a) / (f(b) - f(a))
+                if f(x) * f(a) < 0:
+                    while abs(x - b) > epsi:
+                        b = x
+                        x = a - (b - a) * f(a) / (f(b) - f(a))
+                        #print(a, b, x, f(x))
+                else:
+                    while abs(x - a) > epsi:
+                        a = x
+                        x = a - (b - a) * f(a) / (f(b) - f(a))
+                        #print(a, b, x, f(x))
+                    print (x)
+                    result = "x = " + str(x)
+                    window["result"].update(result)
 
---
+++ PPT DAY CUNG/day cung v3.py
@@ -0,0 +1,60 @@
+import PySimpleGUI as sg
+
+def secant_method(equation, x0, x1, epsilon=None, num_steps=None):
+ 
+    #def f(x):
+        #return eval(equation, {'x': x}) #sử dụng hàm eval để tính toán giá trị của hàm f(x) với x thay thế bằng x giá trị truyền vào
+    
+    f = lambda x: eval(equation, {'x': x})  #sử dụng hàm lambda để viết gọn hàm f(x)
+
+    x_values = [] 
+
+    if epsilon is not None: #nếu epsilon được chọn
+        while True:
+            x2 = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0))
+            x_values.append(x2)
+            if abs(x2 - x1) < epsilon:
+                return x2
+            x0, x1 = x1, x2
+
+    if num_steps is not None: #nếu số bước được chọn
+        for _ in range(num_steps):
+            x2 = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0))
+            x_values.append(x2)
+            x0, x1 = x1, x2
+
+        return x_values
+
+layout = [
+    [sg.Text('Phương trình:'), sg.InputText(key='equation', size=(30, 1),expand_x=True)],
+    [sg.Text('Giá trị ban đầu x0:'), sg.InputText(key='x0', size=(3, 1))],
+    [sg.Text('Giá trị ban đầu x1:'), sg.InputText(key='x1', size=(3, 1))],
+    [sg.Text('Lựa chọn:'), sg.Combo(['Epsilon', 'Số bước'], default_value='Epsilon', key='option')],
+    [sg.Text('Nhập Epsilon/Số bước:', key='param_text'), sg.InputText(key='param_value',size=(7, 1))],
+    [sg.Button('Tính')],
+    [sg.Output(size=(50, 10))]
+]
+
+window = sg.Window('Phương pháp dây cung', layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WINDOW_CLOSED:
+        break
+
+    if event == 'Tính': 
+        equation = values['equation']
+        x0 = float(values['x0'])
+        x1 = float(values['x1'])
+        option = values['option']
+        param_value = float(values['param_value']) if values['param_value'] else None #parametter value = giá trị của epsilon hoặc số bước
+        epsilon = param_value if option == 'Epsilon' else None #epsilon = giá trị của epsilon nếu lựa chọn là epsilon
+        num_steps = int(param_value) if option == 'Số bước' else None #num_steps = giá trị của số bước nếu lựa chọn là số bước
+
+        results = secant_method(equation, x0, x1, epsilon, num_steps)
+
+        for i, x in enumerate(results): #enumerate dạng map/dict trong python, ở đây là (0, x0), (1, x1), (2, x2)
+            print(f'Bước {i+1}: x = {x}')
+
+window.close()
